cab476c84d16ec480e55694a2109595fff7b012b
jhkim <jhkim@hancom.com>
2017-12-07 15:04:51 +0900

    saying data load from external json file.

--- controller.js
+++ controller.js
@@ -1,40 +1,10 @@
 
-angular.module("myApp", []).controller('typingController', function($scope, $interval) {
-    $scope.names = [
-        {data:'가까운 데 집은 깎이고 먼 데 절은 비친다'},
-        {data:'가는 날이 장날'},
-        {data:'가는 말이 고와야 오는 말도 곱다'},
-        {data:'개구리 올챙이 시절 모른다'},
-        {data:'나는 새도 떨어뜨린다'},
-        {data:'방귀 뀐 놈이 성낸다'},
-        {data:'사공이 많으면 배가 산으로 간다'},
-        {data:'사흘 굶어 아니나는 생각 없다'},
-        {data:'손바닥으로 하늘가리기'},
-        {data:'나는 새도 떨어뜨린다'},
+angular.module("myApp", []).controller('typingController', function($scope, $interval, $http) {
 
-        {data:'늦게 배운 도둑질이 날 새는 줄 모른다'},
-        {data:'때리는 시어머니보다 말리는 시누이가 더 밉다'},
-        {data:'도둑이 제 발 저린다'},
-        {data:'못된 송아지 엉덩이에 뿔난다'},
-        {data:'방귀 뀐 놈이 성낸다'},
-        {data:'벙어리 냉가슴 앓듯'},
-        {data:'보기 좋은 떡이 먹기도 좋다'},
-        {data:'비온 뒤 땅이 굳는다'},
-        {data:'빈대 잡다 초가삼간 태운다'},
-        {data:'소 귀에 경 읽기'},
-
-        {data:'아니 땐 굴뚝에 연기나랴'},
-        {data:'어물전 망신은 꼴뚜기가 시킨다'},
-        {data:'울며 겨자 먹기'},
-        {data:'원숭이도 나무에서 떨어진다'},
-        {data:'이미 엎질러진 물이다'},
-        {data:'칼로써 흥한자 칼로써 망한다'},
-        {data:'티끌 모아 태산'},
-        {data:'하늘의 별 따기'},
-        {data:'하늘이 무너저도 솟아날 구멍은 있다'},
-        {data:'호랑이도 제 말 하면 온다'}
-
-    ];
+    $http.get('saying.json').then(function (value) {
+        $scope.sayingData = value.data;
+        $scope.init();
+    });
 
     $scope.outputStyle = {
         "background-color" : "lightskyblue",
@@ -66,8 +36,8 @@ angular.module("myApp", []).controller('typingController', function($scope, $int
     }, 1000);
 
     $scope.init = function () {
-        var index = Math.floor((Math.random() * $scope.names.length) + 1);
-        $scope.inputData = $scope.names[index].data;
+        var index = Math.floor((Math.random() * $scope.sayingData.length) + 1);
+        $scope.inputData = $scope.sayingData[index];
     }
 
     $scope.typing = function ($event) {
@@ -86,7 +56,7 @@ angular.module("myApp", []).controller('typingController', function($scope, $int
 
     $scope.typeDone = function () {
         var index = Math.floor((Math.random() * 10) + 1);
-        $scope.inputData = $scope.names[index].data;
+        $scope.inputData = $scope.sayingData[index];
         $scope.inputDataLength = $scope.inputData.length;
         $scope.outputData = "";
         var takeTime = new Date().getTime() - $scope.startTime;
@@ -120,7 +90,6 @@ angular.module("myApp", []).controller('typingController', function($scope, $int
             return false;
         }
     }
-    $scope.init();
 });
 
 
