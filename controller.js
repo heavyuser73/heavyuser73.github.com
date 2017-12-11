20a77899e5cf7b3060512936433a65f3001df4a2
jhkim <jhkim@hancom.com>
2017-12-11 20:12:14 +0900

    언어 설정 추가 중.

--- controller.js
+++ controller.js
@@ -1,17 +1,56 @@
 'use strict';
 
-var app = angular.module("myApp", ['ngRoute']);
+var app = angular.module("myApp", ['ngRoute', 'pascalprecht.translate', 'ngSanitize', 'ngCookies']);
 
-app.controller('mainController', function($scope) {
-    $scope.langs = [{name:"한글", value:"data/saying_ko.json"},
-       {name:"English", value:"data/saying_en.json"}];
+app.controller('mainController', function($scope, $translate) {
+    $scope.langs = [{name:"한글", value:"data/saying_ko.json", langKey:"ko"},
+       {name:"English", value:"data/saying_en.json", langKey:"en"}];
 
     var defaultLangPath = "data/saying_ko.json";
     setCookie("langPath", defaultLangPath, 365);
     $scope.selectedLangChanged = function () {
         setCookie("langPath", $scope.selectLang.value, 365);
+        $translate.use($scope.selectLang.langKey);
     }
 });
+
+
+var translationsEN = {
+    SAYING_TYPING_MAIN_TITLE: 'Saying Typing Exercise',
+    SAYING_TYPING_SUB_TITLE: "Let's practice typing!",
+    TYPING_EXERCISE: 'Typing exercise',
+    TYPING_EXERCISE_EXPAIN: 'It is a place where anyone can practice typing.',
+    KOREAN: 'Hangul',
+    ENGLISH: 'English',
+    START: 'Start',
+    DEVELOPER: 'Developer',
+    GO_HOME : 'Go home',
+    SAYING : 'Saying',
+    INPUT_SAYING : "Input Saying",
+    SPEED_PER_MINUTE : "Speed per minute",
+    MAX_SPEED_PER_MINUTE : "Max speed per minute",
+    HOURS : "Hour",
+    SECONDS : "Second"
+  };
+  
+  var translationsKO= {
+    SAYING_TYPING_MAIN_TITLE: '속담 타자 연습',
+    SAYING_TYPING_SUB_TITLE: '타자 연습 열심히 해 봅시다!',
+    TYPING_EXERCISE: '타자 연습',
+    TYPING_EXERCISE_EXPAIN: '누구나 타자 연습을 할 수 있는 곳입니다.',
+    KOREAN: '한글',
+    ENGLISH: 'English',
+    START: '시작',
+    DEVELOPER: '만든사람',
+    GO_HOME : '홈으로',
+    SAYING : '속담',
+    INPUT_SAYING : "속담을 입력 하세요",
+    SPEED_PER_MINUTE : "분당 속도",
+    MAX_SPEED_PER_MINUTE : "분당 최고 속도",
+    HOURS : "시간",
+    SECONDS : "초"
+  };
+
 
 app.controller('typingController', function($scope, $interval, $http) {
     $scope.returnAudio = new Audio('res/typewriter-line-break.mp3');
@@ -126,7 +165,18 @@ app.config(function($routeProvider) {
     .otherwise({redirectTo: '/'});
 });
 
+app.config(['$translateProvider', function ($translateProvider) {
+    // add translation tables
+    $translateProvider.translations('en', translationsEN);
+    $translateProvider.translations('ko', translationsKO);
+    $translateProvider.fallbackLanguage('ko');
+    $translateProvider.preferredLanguage('ko');   
+    // Enable escaping of HTML
+    $translateProvider.useSanitizeValueStrategy('sanitize');
+    $translateProvider.useLocalStorage();
+  }]);
 
+  
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
